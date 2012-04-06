9396fc1f326e782e258851da5f89b0c76ceab3a0
amaan <amaan@localhost>
2012-04-06 10:19:46 +0100

    postbuild fix

--- Shaders/MentalRay/aaOcean/postBuild.bat
+++ Shaders/MentalRay/aaOcean/postBuild.bat
@@ -1,12 +1,11 @@
 SET XSI_INSTALL_DIR=C:\Users\amaan\Autodesk\Softimage_Winchester_RC1\Application
 
-copy spdls\aaOceanDisplaceShader.spdl "%XSI_INSTALL_DIR%"\spdl
-copy spdls\aaOceanFoamShader.spdl "%XSI_INSTALL_DIR%"\spdl
-copy spdls\aaOceanImgVecDisplace.spdl "%XSI_INSTALL_DIR%"\spdl
+copy Softimage\spdls\aaOceanDisplaceShader.spdl "%XSI_INSTALL_DIR%"\spdl
+copy Softimage\spdls\aaOceanFoamShader.spdl "%XSI_INSTALL_DIR%"\spdl
+copy Softimage\spdls\aaOceanImgVecDisplace.spdl "%XSI_INSTALL_DIR%"\spdl
 REM copy spdls\aaOceanNormalsShader.spdl "%XSI_INSTALL_DIR%"\spdl
 
 copy ..\Output\aaOceanMentalRay.dll "%XSI_INSTALL_DIR%"\bin\nt-x86-64
 
 del ..\Output\aaOceanMentalRay.exp
 del ..\Output\aaOceanMentalRay.lib
-rd /S /Q x64
