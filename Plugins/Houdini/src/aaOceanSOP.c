c9c1b679201a68d1b41f88d82a0f13d37898d83a
amaan <amaan@localhost>
2012-12-31 11:19:07 +0000

    [Houdini] Code cleanup/commenting

--- Plugins/Houdini/src/aaOceanSOP.c
+++ Plugins/Houdini/src/aaOceanSOP.c
@@ -1,4 +1,9 @@
-
+// aaOcean v2.6 Houdini SOP Deformer
+// Author: Amaan Akram 
+// www.amaanakram.com
+// aaOcean is free software and can be redistributed and modified under the terms of the 
+// GNU General Public License (Version 3) as provided by the Free Software Foundation.
+// GNU General Public License http://www.gnu.org/licenses/gpl.html
 
 #ifdef MSVC_DEFINES // defined in MSVC project settings, preprocessor section
 #define VERSION "12.1.125"
@@ -13,6 +18,7 @@
 #define AMD64 
 #define SIZEOF_VOID_P 8
 #define MAKING_DSO
+#define SESI_TAGINFO "Produced by: Amaan Akram"
 #endif
 
 #include <UT/UT_DSOVersion.h>
@@ -31,148 +37,168 @@
 
 void newSopOperator(OP_OperatorTable *table)
 {
-	table->addOperator(new OP_Operator("aaOceanSOP",
-		"aaOceanSOP",
-		aaOceanSOP::myConstructor,
-		aaOceanSOP::myTemplateList,
-		1,
-		1,
-		0));
+    table->addOperator(new OP_Operator("aaOceanSOP",
+        "aaOceanSOP",
+        aaOceanSOP::myConstructor,
+        aaOceanSOP::myTemplateList,
+        1,
+        1,
+        0));
 }
 
 static PRM_Name names[] = 
 {
-	PRM_Name("resolution",		"Resolution"),
-	PRM_Name("seed",			"Seed"),
-	PRM_Name("oceanScale",		"Ocean Scale"),
+    PRM_Name("resolution",		"Resolution"),
+    PRM_Name("seed",			"Seed"),
+    PRM_Name("oceanScale",		"Ocean Scale"),
 
-	PRM_Name("velocity",		"Wave Size"),
-	PRM_Name("cutoff",			"Wave Smooth"),
-	PRM_Name("windDir",			"Wind Dir"),
-	PRM_Name("windAlign",		"Wind Align"),
+    PRM_Name("velocity",		"Wave Size"),
+    PRM_Name("cutoff",			"Wave Smooth"),
+    PRM_Name("windDir",			"Wind Dir"),
+    PRM_Name("windAlign",		"Wind Align"),
 
-	PRM_Name("damp",			"Reflected Waves"),
-	PRM_Name("waveSpeed",		"Wave Speed"),
-	PRM_Name("waveHeight",		"Wave Height"),
-	PRM_Name("chop",			"Chop Amount"),
-	PRM_Name("enableFoam",		"enableFoam"),
-	PRM_Name("timeOffset",		"Time Offset"),
+    PRM_Name("damp",			"Reflected Waves"),
+    PRM_Name("waveSpeed",		"Wave Speed"),
+    PRM_Name("waveHeight",		"Wave Height"),
+    PRM_Name("chop",			"Chop Amount"),
+    PRM_Name("enableFoam",		"enableFoam"),
+    PRM_Name("timeOffset",		"Time Offset"),
+    PRM_Name("uvAttribute",		"UV Attribute"),
 
 };
 
 PRM_Template aaOceanSOP::myTemplateList[] = 
-{
-	//PRM_Template(PRM_STRING,  1, &PRMgroupName, 0, &SOP_Node::pointGroupMenu),
-	PRM_Template(PRM_INT_E,	1, &names[0], PRMfourDefaults, 0, &PRMfrequencyRange),	// resolution
-	PRM_Template(PRM_INT_E,	1, &names[1], PRMoneDefaults,  0, &PRMrolloffRange),	// seed
-	PRM_Template(PRM_FLT_J,	1, &names[2], PRM100Defaults,  0, &PRMpositiveRange),	// oceanScale
+{	
+    PRM_Template(PRM_INT_E,	1, &names[0],  PRMfourDefaults, 0, &PRMfrequencyRange),		// resolution
+    PRM_Template(PRM_FLT_J,	1, &names[2],  PRM100Defaults,  0, &PRMpositiveRange),		// oceanScale
+    PRM_Template(PRM_INT_E,	1, &names[1],  PRMoneDefaults,  0, &PRMfrequency10Range),	// seed
+    PRM_Template(PRM_FLT_J,	1, &names[12], PRMzeroDefaults, 0, &PRMscaleRange),			// timeOffset
 
-	PRM_Template(PRM_FLT_J,	1, &names[3], PRMfourDefaults, 0, &PRMdivision0Range),	// velocity
-	PRM_Template(PRM_FLT_J,	1, &names[4], PRMzeroDefaults, 0, &PRMdivision0Range),	// cutoff
-	PRM_Template(PRM_FLT_J,	1, &names[5], PRMzeroDefaults, 0, &PRMangleRange),		// windDir
-	PRM_Template(PRM_INT_E,	1, &names[6], PRMzeroDefaults, 0, &PRMdivision0Range),	// windAlign
+    PRM_Template(PRM_FLT_J,	1, &names[9],  PRMoneDefaults,  0, &PRMdivision0Range),		// waveHeight
+    PRM_Template(PRM_FLT_J,	1, &names[3],  PRMfourDefaults, 0, &PRMdivision0Range),		// velocity (Wave Size)
+    PRM_Template(PRM_FLT_J,	1, &names[8],  PRMoneDefaults,  0, &PRMdivision0Range),		// waveSpeed
+    PRM_Template(PRM_FLT_J,	1, &names[10], PRMzeroDefaults, 0, &PRMrolloffRange),		// chop
+    PRM_Template(PRM_FLT_J,	1, &names[4],  PRMzeroDefaults, 0, &PRMdivision0Range),		// cutoff (Wave Smooth)
 
-	PRM_Template(PRM_FLT_J,	1, &names[7], PRMzeroDefaults, 0, &PRMunitRange),		// damp
-	PRM_Template(PRM_FLT_J,	1, &names[8], PRMoneDefaults,  0, &PRMdivision0Range),	// waveSpeed
-	PRM_Template(PRM_FLT_J,	1, &names[9], PRMoneDefaults,  0, &PRMdivision0Range),	// waveHeight
-	PRM_Template(PRM_FLT_J,	1, &names[10], PRMzeroDefaults, 0, &PRMlodRange),		// chop
-	PRM_Template(PRM_TOGGLE,1, &names[11]),										// enable Foam
-	PRM_Template(PRM_FLT_J,	1, &names[12], PRMzeroDefaults, 0, &PRMscaleRange),		// timeOffset
+    PRM_Template(PRM_FLT_J,	1, &names[5],  PRMzeroDefaults, 0, &PRMangleRange),			// windDir
+    PRM_Template(PRM_FLT_J,	1, &names[7],  PRMzeroDefaults, 0, &PRMunitRange),			// damp
+    PRM_Template(PRM_INT_E,	1, &names[6],  PRMzeroDefaults, 0, &PRMdivision0Range),		// windAlign
 
-	PRM_Template(),
+    PRM_Template(PRM_TOGGLE,1, &names[11]),												// enable Foam
+    PRM_Template(PRM_STRING,1, &names[13], 0),											// UV Attribute
+
+    PRM_Template(),
 };
 
 
 OP_Node *aaOceanSOP::myConstructor(OP_Network *net, const char *name, OP_Operator *op)
 {
-	return new aaOceanSOP(net, name, op);
+    return new aaOceanSOP(net, name, op);
 }
 
 aaOceanSOP::aaOceanSOP(OP_Network *net, const char *name, OP_Operator *op)
-	: SOP_Node(net, name, op), myGroup(0)
+    : SOP_Node(net, name, op)
 {
-	// Make sure to flag that we can supply a guide geometry
-	pOcean = new aaOcean;
-	mySopFlags.setNeedGuide1(0);
+    pOcean = new aaOcean;
 }
 
 aaOceanSOP::~aaOceanSOP() 
 {
-	if(pOcean)
-		delete pOcean;
+    if(pOcean)
+        delete pOcean;
 }
-
 
 OP_ERROR aaOceanSOP::cookMySop(OP_Context &context)
 {
-	fpreal		now		= context.getTime();
-	int			npts	= gdp->points().entries();
-	UT_Vector3	UVvalue;
-	UT_Vector4	Pvalue;
-	float		u,v;
-	GEO_AttributeHandle	UVhandle, Phandle;
+    fpreal		now		= context.getTime();
+    int			npts	= gdp->points().entries();
+    UT_Vector4	PtValue;
+    float		u, v;
+    UT_String	UvAttribute;
+    GEO_AttributeHandle	UvHandle, PtHandle;
 
+    if (lockInputs(context) >= UT_ERROR_ABORT)
+        return error();
 
-	if (lockInputs(context) >= UT_ERROR_ABORT)
-		return error();
-
-	duplicateSource(0, context);
-
-	setVariableOrder(3, 2, 0, 1);
+    duplicateSource(0, context);
+    setVariableOrder(3, 2, 0, 1);
     setCurGdh(0, myGdpHandle);
     setupLocalVars();
 
-	// Flag the SOP as being time dependent (i.e. cook on time changes)
-	flags().timeDep = 1;
+    // Flag the SOP as being time dependent (i.e. cook on time changes)
+    flags().timeDep = 1;
 
-	pOcean->input(	RESOLUTION(), 
-					SEED(),
-					OCEANSCALE(now), 
-					VELOCITY(now), 
-					CUTOFF(now), 
-					WINDDIR(now), 
-					WINDALIGN(), 
-					DAMP(now), 
-					WAVESPEED(now), 
-					WAVEHEIGHT(now),
-					CHOP(now), 
-					now,
-					(bool)ENABLEFOAM());
+    // start pulling in SOP inputs and send to aaOcean 
+    now = now + TIMEOFFSET(now);
+    pOcean->input(	RESOLUTION(), 
+                    SEED(),
+                    OCEANSCALE(now), 
+                    VELOCITY(now), 
+                    CUTOFF(now), 
+                    WINDDIR(now), 
+                    WINDALIGN(), 
+                    DAMP(now), 
+                    WAVESPEED(now), 
+                    WAVEHEIGHT(now),
+                    CHOP(now), 
+                    now,
+                    (bool)ENABLEFOAM());
 
-	UVhandle = gdp->getAttribute(GEO_POINT_DICT,"uv");
-	if(pOcean->isValid() && UVhandle.isAttributeValid())
-	{
-		#pragma omp parallel for private(UVvalue, Pvalue, u, v, UVhandle, Phandle)
-		for (int i = 0; i < npts; ++i)
-		{
-			UVhandle = gdp->getAttribute(GEO_POINT_DICT,"uv");
-			Phandle  = gdp->getAttribute(GEO_POINT_DICT, "P");
+    if(!pOcean->isValid())
+    {
+        unlockInputs();
+        return error();
+    }
 
-			UVhandle.setElement(gdp->points()(i));
-			UVvalue = UVhandle.getV3();
-			u = UVvalue.x();
-			// Houdini V coord runs in opposite direction compared to Softimage/Maya
-			// conforming with other apps to make ocean shape consistent across apps
-			v = 1.0 - UVvalue.y();
+    // get the user-specified attribute that holds uv-data
+    // check that if it is valid
+    getUVAttribute(UvAttribute);
+    if(UvAttribute.length() == 0)
+        UvAttribute = "uv";
+    const char* attrib = (const char *)UvAttribute;
+    UvHandle = gdp->getAttribute(GEO_POINT_DICT, attrib);
 
-			Phandle.setElement(gdp->points()(i));
-			Pvalue = Phandle.getV3();
+    if(!UvHandle.isAttributeValid())
+    {
+        // uv attribute not found
+        unlockInputs();
+        return error();
+    }
+    
+    // inputs validated. Begin sending ocean data to 
+    #pragma omp parallel for private(PtHandle, PtValue, u, v, UvHandle)
+    for (int i = 0; i < npts; ++i)
+    {
+        UvHandle = gdp->getAttribute(GEO_POINT_DICT, attrib);
+        PtHandle  = gdp->getAttribute(GEO_POINT_DICT, "P");
 
-			Pvalue.y() += pOcean->getOceanData(u, v, aaOcean::eHEIGHTFIELD);
-			if(pOcean->isChoppy())
-			{
-				Pvalue.x() += pOcean->getOceanData(u, v, aaOcean::eCHOPX);
-				Pvalue.z() += pOcean->getOceanData(u, v, aaOcean::eCHOPZ);
-			}
-			Phandle.setV3(Pvalue);
-		}
-	}
-	unlockInputs();
-	return error();
+        UvHandle.setElement(gdp->points()(i));
+        PtHandle.setElement(gdp->points()(i));
+
+        u = UvHandle.getV3().x();
+        // Houdini V coord runs in opposite direction compared to Softimage/Maya
+        // Conforming with other apps to make ocean shape consistent across apps
+        v = 1.0 - (fmod(UvHandle.getV3().y(), 1.0f));
+
+        PtValue = PtHandle.getV3();
+
+        PtValue.y() += pOcean->getOceanData(u, v, aaOcean::eHEIGHTFIELD);
+        if(pOcean->isChoppy())
+        {
+            PtValue.x() += pOcean->getOceanData(u, v, aaOcean::eCHOPX);
+            PtValue.z() += pOcean->getOceanData(u, v, aaOcean::eCHOPZ);
+        }
+        PtHandle.setV3(PtValue);
+    }
+
+    // Notify the display cache
+    gdp->notifyCache(GU_CACHE_ALL);
+    unlockInputs();
+    return error();
 }
 
 
 const char *aaOceanSOP::inputLabel(unsigned) const
 {
-	return "UV'ed Geometry to simulate ocean on";
+    return "UV'ed Geometry to simulate ocean on";
 }
--- Plugins/Houdini/src/aaOceanSOP.h
+++ Plugins/Houdini/src/aaOceanSOP.h
@@ -1,3 +1,9 @@
+// aaOcean v2.6 Houdini SOP Deformer
+// Author: Amaan Akram 
+// www.amaanakram.com
+// aaOcean is free software and can be redistributed and modified under the terms of the 
+// GNU General Public License (Version 3) as provided by the Free Software Foundation.
+// GNU General Public License http://www.gnu.org/licenses/gpl.html
 
 #ifndef __aaOceanSOP_h__
 #define __aaOceanSOP_h__
@@ -8,45 +14,47 @@
 class aaOceanSOP : public SOP_Node
 {
 public:
-	aaOceanSOP(OP_Network *net, const char *name, OP_Operator *op);
-	virtual ~aaOceanSOP();
+    aaOceanSOP(OP_Network *net, const char *name, OP_Operator *op);
+    virtual ~aaOceanSOP();
 
-	static PRM_Template myTemplateList[];
-	static OP_Node *myConstructor(OP_Network*, const char *, OP_Operator *);
+    static PRM_Template myTemplateList[];
+    static OP_Node *myConstructor(OP_Network*, const char *, OP_Operator *);
 
 protected:
-	virtual const char	*inputLabel(unsigned idx) const;
+    virtual const char	*inputLabel(unsigned idx) const;
 
-	/// Method to cook geometry for the SOP
-	virtual OP_ERROR cookMySop(OP_Context &context);
+    /// Method to cook geometry for the SOP
+    virtual OP_ERROR cookMySop(OP_Context &context);
 
 private:
-	//void	getGroups(UT_String &str)	{ evalString(str, "group", 0, 0); }
-	int		RESOLUTION()			{ return evalInt("resolution", 0, 0); }
-	int		SEED()					{ return evalInt("seed", 0, 0); }
-	fpreal	OCEANSCALE(fpreal t)	{ return evalFloat("oceanScale", 0, t); }
+    //void	getGroups(UT_String &str)	{ evalString(str, "group", 0, 0); }
+    int		RESOLUTION()			{ return evalInt("resolution", 0, 0); }
+    int		SEED()					{ return evalInt("seed", 0, 0); }
+    fpreal	OCEANSCALE(fpreal t)	{ return evalFloat("oceanScale", 0, t); }
 
-	fpreal	VELOCITY(fpreal t)		{ return evalFloat("velocity", 0, t); }
-	fpreal	CUTOFF(fpreal t)		{ return evalFloat("cutoff", 0, t); }
-	fpreal	WINDDIR(fpreal t)		{ return evalFloat("windDir", 0, t); }
-	int		WINDALIGN()				{ return evalInt("windAlign", 0, 0); }
+    fpreal	VELOCITY(fpreal t)		{ return evalFloat("velocity", 0, t); }
+    fpreal	CUTOFF(fpreal t)		{ return evalFloat("cutoff", 0, t); }
+    fpreal	WINDDIR(fpreal t)		{ return evalFloat("windDir", 0, t); }
+    int		WINDALIGN()				{ return evalInt("windAlign", 0, 0); }
 
-	fpreal	DAMP(fpreal t)			{ return evalFloat("damp", 0, t); }
-	fpreal	WAVESPEED(fpreal t)		{ return evalFloat("waveSpeed", 0, t); }
-	fpreal	WAVEHEIGHT(fpreal t)	{ return evalFloat("waveHeight", 0, t); }
-	fpreal	CHOP(fpreal t)			{ return evalFloat("chop", 0, t); }
-	int		ENABLEFOAM()			{ return evalInt("enableFoam", 0, 0); }
-	fpreal	TIMEOFFSET(fpreal t)	{ return evalFloat("timeOffset", 0, t); }
-	
-	/// This variable is used together with the call to the "checkInputChanged"
-	/// routine to notify the handles (if any) if the input has changed.
-	GU_DetailGroupPair	 myDetailGroupPair;
+    fpreal	DAMP(fpreal t)			{ return evalFloat("damp", 0, t); }
+    fpreal	WAVESPEED(fpreal t)		{ return evalFloat("waveSpeed", 0, t); }
+    fpreal	WAVEHEIGHT(fpreal t)	{ return evalFloat("waveHeight", 0, t); }
+    fpreal	CHOP(fpreal t)			{ return evalFloat("chop", 0, t); }
+    int		ENABLEFOAM()			{ return evalInt("enableFoam", 0, 0); }
+    fpreal	TIMEOFFSET(fpreal t)	{ return evalFloat("timeOffset", 0, t); }
 
-	/// This is the group of geometry to be manipulated by this SOP and cooked
-	/// by the method "cookInputGroups".
-	const GA_PointGroup	*myGroup;
+    void	getUVAttribute(UT_String &str){ evalString(str, "uvAttribute", 0, 0); }
+    void	getEigenPlusAttribute(UT_String &str){ evalString(str, "eigenPlusAttribute", 0, 0); }
+    void	getEigenMinusAttribute(UT_String &str){ evalString(str, "eigenMinusAttribute", 0, 0); }
+    void	getEigenValueAttribute(UT_String &str){ evalString(str, "eigenValueAttribute", 0, 0); }
+    
+    /// This variable is used together with the call to the "checkInputChanged"
+    /// routine to notify the handles (if any) if the input has changed.
+    GU_DetailGroupPair	 myDetailGroupPair;
 
-	aaOcean *pOcean;
+    // pointer to our aaOcean class
+    aaOcean *pOcean;
 };
 
 #endif
