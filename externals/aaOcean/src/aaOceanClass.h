bc82d957da172a5254004f58dcd29472393c2473
amaanakram <amaanakram@localhost>
2014-12-20 12:11:31 +0000

    [aaocean] using kissfft. fftw removed

--- externals/aaOcean/src/aaOceanClass.h
+++ externals/aaOcean/src/aaOceanClass.h
@@ -8,7 +8,10 @@
 #ifndef AAOCEANCLASS_H
 #define AAOCEANCLASS_H
 
-#include "fftw3.h"
+#include "kissfft/kiss_fft.h"
+#include "kissfft/kiss_fft.c"
+#include "kissfft/tools/kiss_fftnd.h"
+#include "kissfft/tools/kiss_fftnd.c"
 
 class aaOcean
 { 
@@ -26,7 +29,7 @@ public:
        eEIGENPLUSX,
        eEIGENPLUSZ,
        eEIGENMINUSX,
-       eEIGENMINUSZ,
+       eEIGENMINUSZ
     };
 
     void input( int     resolution,
@@ -88,6 +91,16 @@ public:
     float   *m_rand1;
     float   *m_rand2;
 
+    //ocean output array pointers
+    float *m_out_fft_htField; // y displacement
+    float *m_out_fft_chopX;   // x displacement
+    float *m_out_fft_chopZ;   // z displacement
+    float *m_out_fft_jxxX; // eigenvector X component
+    float *m_out_fft_jxxZ; // eigenvector Z component
+    float *m_out_fft_jzzX; // eigenvector X component
+    float *m_out_fft_jzzZ; // eigenvector Z component
+    float *m_out_fft_jxz;  // eigenvalue
+
     //bool types for various checks during run-time
     bool    m_isAllocated;
     bool    m_isFoamAllocated;
@@ -99,19 +112,19 @@ public:
     // memory tracking
     int     m_memory;
 
-    fftwf_complex *m_fft_htField;
-    fftwf_complex *m_fft_chopX;
-    fftwf_complex *m_fft_chopZ;
-    fftwf_complex *m_fft_jxx; // eigenvector
-    fftwf_complex *m_fft_jzz; // eigenvector
-    fftwf_complex *m_fft_jxz; // eigenvalue
+    kiss_fft_cpx *m_fft_htField;
+    kiss_fft_cpx *m_fft_chopX;
+    kiss_fft_cpx *m_fft_chopZ;
+    kiss_fft_cpx *m_fft_jxx; // eigenvector
+    kiss_fft_cpx *m_fft_jzz; // eigenvector
+    kiss_fft_cpx *m_fft_jxz; // eigenvalue
 
-    fftwf_plan m_planHeightField;
-    fftwf_plan m_planChopX;
-    fftwf_plan m_planChopZ;
-    fftwf_plan m_planJxx;
-    fftwf_plan m_planJxz;
-    fftwf_plan m_planJzz;
+    kiss_fftnd_cfg m_planHeightField;
+    kiss_fftnd_cfg m_planChopX;
+    kiss_fftnd_cfg m_planChopZ;
+    kiss_fftnd_cfg m_planJxx;
+    kiss_fftnd_cfg m_planJxz;
+    kiss_fftnd_cfg m_planJzz;
 
     char m_state[512]; // array for holding the current state of aaOcean object
     
@@ -122,7 +135,7 @@ public:
     
     // initialization functions
     void reInit(int data_size, int seed);
-    unsigned int generateUID(float, float);
+    unsigned int generateUID(const float, const float) const;
     void prepareOcean();
     void setupGrid();
 
@@ -131,12 +144,13 @@ public:
     void evaluateHieghtField();
     void evaluateChopField();
     void evaluateJacobians();
+    void evaluateNormal();
     
     // interpolation functions
     inline float catmullRom(const float t, const float a, const float b, const float c, const float d) const;
     inline int wrap(int x) const;
 
-    void getArrayType(aaOcean::arrayType type, fftwf_complex*& outArray, int &arrayIndex) const;
+    void getArrayType(aaOcean::arrayType type, float*& outArray) const;
 };
 
 #endif  /* AAOCEANCLASS_H */
